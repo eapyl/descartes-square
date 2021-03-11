e4f097970209a4439ce14ac912200111d9bdce53
eapyl <gromkaktus@gmail.com>
2021-03-11 21:24:14 +0100

    Move to root

--- src.5ecc0399.js
+++ src.5ecc0399.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";var e=require("./Main.elm");e.Elm.Main.init({node:document.querySelector("main")});
 },{"./Main.elm":"8asW"}]},{},["Focm"], null)
-//# sourceMappingURL=descartes-square/src.5ecc0399.js.map
+//# sourceMappingURL=src.5ecc0399.js.map
